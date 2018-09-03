515c7360fb67afe014f586ea2a813333d06bc475
oooopfoooo <408445889@qq.com>
2018-09-03 19:43:36 +0800

    file

---
+++ HDFS.py
@@ -0,0 +1,35 @@
+from config import *
+
+hdfs = Blueprint('hdfs', __name__)
+
+@hdfs.route('/getFiles', methods=['GET'])
+def getFiles():
+    try:
+        data = []
+        path =request.values.get('path')
+        owner = request.values.get('owner')
+        group = request.values.get('group')
+        f = file()
+        data = f.getFiles(path,owner,group)
+        res = json.dumps(data, ensure_ascii=False)
+        resp = jsonify(res)
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
+    except Exception as e:
+        print(e)
+        return get_error_resp(e)
+
+@hdfs.route('/deleteFiles', methods=['DELETE'])
+def deleteFiles():
+    try:
+        data = {}
+        path =request.values.get('path')
+        f = file()
+        data = f.deleteFiles(path)
+        res = json.dumps(data, ensure_ascii=False)
+        resp = jsonify(res)
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
+    except Exception as e:
+        print(e)
+        return get_error_resp(e)
