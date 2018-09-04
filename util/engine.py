6bfee6839f18501cc444f5b8235a2859ff6f0f77
liwan <1349654484@qq.com>
2018-09-04 15:09:28 +0800

    add get table struction api update upload csv api

--- util/engine.py
+++ util/engine.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 
 def get_mysql_engine(mysql_args):
-    engine = create_engine('mysql+pymysql://%s:%s@%s:3306/%s'%
-                       (mysql_args['user'], mysql_args['passwd'], mysql_args['host'], mysql_args['dbname']),
+    engine = create_engine('mysql+pymysql://%s:%s@%s:%d/%s'%
+                       (mysql_args['user'], mysql_args['passwd'], mysql_args['host'],  mysql_args['port'], mysql_args['dbname']),
+                           connect_args={'charset': 'utf8'})
+    return engine
+
+def get_other_engine(mysql_args):
+    engine = create_engine('mysql+pymysql://%s:%s@%s:%d/%s'%
+                       (mysql_args['user'], mysql_args['passwd'], mysql_args['host'],  mysql_args['port'], mysql_args['dbname']),
                            connect_args={'charset': 'utf8'})
     return engine
