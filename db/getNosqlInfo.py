0488c3374add14c962b62d15fe91cfaa74f43f4d
BJJ-zhangyufei <1535256336@qq.com>
2018-09-19 11:15:07 +0800

    提取Cassandra字段

---
+++ db/getNosqlInfo.py
@@ -0,0 +1,30 @@
+import os
+from cassandra.cluster import Cluster
+
+class NoSQLTool():
+
+    def __init__(self):
+        self.cluster = Cluster(contact_points=['39.104.165.155'])
+        self.session = self.cluster.connect('bupt_iot')
+
+    def get_columns(self):
+        try:
+            sql = "select table_name from system_schema.tables where keyspace_name = 'bupt_iot'"
+            keywords = self.session.execute(sql)
+            #print(keywords)
+            for row in keywords:
+                print(row)
+                query = "select * from system_schema.columns where keyspace_name = 'bupt_iot' and  table_name = '%s'"%(row)
+                rs = self.session.execute(query)
+                #print(rs)
+                for rs in rs:
+                    print(rs)
+        except Exception as e:
+            print(e)
+
+
+
+
+if __name__ == '__main__':
+    NoSQLTool().get_columns()
+
